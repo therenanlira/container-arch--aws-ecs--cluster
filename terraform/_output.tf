48bf8e900e55e3d5b1ae10e9b13ccaca54a42b6b
Renan Lira <22295448+therenanlira@users.noreply.github.com>
2026-08-07 00:11:29 -0300

    aula10 (#7)
    * aula10
    
    * aula10

--- terraform/_output.tf
+++ terraform/_output.tf
@@ -18,8 +18,8 @@ output "lb_listener_arn" {
   value = module.ecs_cluster.lb_listener_arn
 }
 
-output "cloudmap" {
-  value = module.ecs_cluster.cloudmap
+output "cloudmap_id" {
+  value = module.ecs_cluster.cloudmap_id
 }
 
 output "cloudmap_name" {
@@ -28,4 +28,16 @@ output "cloudmap_name" {
 
 output "cloudmap_arn" {
   value = module.ecs_cluster.cloudmap_arn
+}
+
+output "service_connect_id" {
+  value = module.ecs_cluster.service_connect_id
+}
+
+output "service_connect_name" {
+  value = module.ecs_cluster.service_connect_name
+}
+
+output "service_connect_arn" {
+  value = module.ecs_cluster.service_connect_arn
 }

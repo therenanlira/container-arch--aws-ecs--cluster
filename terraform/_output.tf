2395bfbcbfb29901655dbdc96bcd12a19a03fada
Renan Lira <22295448+therenanlira@users.noreply.github.com>
2026-08-06 22:59:35 -0300

    aula9: restarting (#6)

--- terraform/_output.tf
+++ terraform/_output.tf
@@ -10,6 +10,22 @@ output "lb_dns_name" {
   value = module.ecs_cluster.lb_dns_name
 }
 
+output "lb_zone_id" {
+  value = module.ecs_cluster.lb_zone_id
+}
+
 output "lb_listener_arn" {
   value = module.ecs_cluster.lb_listener_arn
+}
+
+output "cloudmap" {
+  value = module.ecs_cluster.cloudmap
+}
+
+output "cloudmap_name" {
+  value = module.ecs_cluster.cloudmap_name
+}
+
+output "cloudmap_arn" {
+  value = module.ecs_cluster.cloudmap_arn
 }

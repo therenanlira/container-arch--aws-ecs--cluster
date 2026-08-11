e33a1d2986e30bd74b773626c85d7843f388f16e
Renan Lira <22295448+therenanlira@users.noreply.github.com>
2026-08-11 20:28:54 -0300

    aula15 (#11)

--- terraform/_remote_state.tf
+++ terraform/_remote_state.tf
@@ -6,3 +6,14 @@ data "terraform_remote_state" "aws_vpc" {
     region = "us-east-2"
   }
 }
+
+data "terraform_remote_state" "aws_ecs_cluster_central" {
+  count = local.workspace.enable_gax && !local.workspace.is_central ? 1 : 0
+
+  backend = "s3"
+  config = {
+    bucket = "150100906110--terraform-backend"
+    key    = "env:/${local.central_workspace}/container-arch/aws-ecs-cluster/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
